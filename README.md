7b123ed1aa68b6f5d7628c42f97388bcd3495560
Erwan Renaudo <r1d1@users.noreply.github.com>
2015-09-08 22:16:29 +0200

    Logging and what to do in readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # coffeeStats
 Doing some plotting and opinion evaluation on coffee making in the office
+
+Commands :
+
+/add : add a new coffee recipe 
+waterLevel(small cup)
+coffeeLevel(spoon)
+coffeeBrand(from dict)
+madeby(userlist):
+> Generate coffee ID
+
+/list <n>: list last n coffees
+
+/eval <ID> : give evaluation on the coffee made
+Taste : crap, bad, ok, good, excellent (converted to 1,5)
+Strength : water, weak, good, strong, harsh (converted to -2,2)
+
+/show : display plot of coffees
+
+/details ID : show details of coffee and history of eval
---
+++ server.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+# This is server.py file
+# from  http://www.tutorialspoint.com/python/python_networking.htm
+# and mostly from http://www.binarytides.com/code-chat-application-server-client-sockets-python/
+# and http://www.binarytides.com/python-socket-programming-tutorial/
+
+import socket               # Import socket module
+import sys
+from thread import *
+import numpy as np
+from serverFunctions import *
+from optparse import OptionParser
+import datetime
+
+now = datetime.datetime.now()
+
+# ====================================================================================
+# Processing arguments :
+#default port :
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345 
+logging=False
+
+parser = OptionParser()
+parser.add_option("-p", "--port", action="store", type="string", dest="port")
+parser.add_option("-L", "--log", action="store_true", dest="logging")
+(options, args) = parser.parse_args()
+print options, args, options.port
+port = int(options.port)
+logging = options.logging
+# ====================================================================================
+print "Trying to start server on", host, ":", port
+# Trying to bind the socket :
+try:
+	s.bind((host, port))        # Bind to the port
+except socket.error , msg:
+	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+	sys.exit()
+# ====================================================================================
+print "Does we log ? ", logging
+logFileName = "coffeestats_log"+"{0}{1}{2}-{3}{4}".format(now.year,now.month,now.day,now.hour,now.minute,now.second)+".txt"
+if logging:
+	logFile = open(logFileName, 'w')
+
+# Now wait for client connection. We should investigate the argument value
+s.listen(10)
+
+print "Socket listening ... ", host
+
+nbOfConnections=0;
+
+#productList=["banana", "apple", "grape", "apricot", "mutton"]
+#productStocks=[100, 90, 50, 70, 30]
+#basePrice = 3.5
+#priceList= np.trunc(100*(basePrice+np.random.randn(len(productList))))/100.0
+##priceList= 2.0+np.around(np.random.randn(len(productList)), decimals=2)
+#priceList = priceList.tolist()
+#
+#print productList
+#print priceList
+#print productStocks
+#productData=zip(*[productList, productStocks, priceList])
+#print productData
+##productData=[{'name':}]
+
+while True:
+	c, addr = s.accept()     # Establish connection with client.
+	nbOfConnections += 1;
+	#print 'Got connection from', addr, ' (', nbOfConnections,' people connected)'
+	#start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+	start_new_thread(clientThread ,(c, []))
+	#start_new_thread(clientThread ,(c, productData))
+	#c.send('Thank you for connecting '+str(np.random.rand()))
+	#c.close()                # Close the connection
+
+print "Server closing ..."
+s.close()
+
+if logging:
+	logFile.close()
